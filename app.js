f8e03cd37d3d14556c1386ec52a756c0598a3629
dougachrist <dougachrist@gmail.com>
2016-06-20 14:21:47 -0700

    added totals

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var arrayTotal = 0;
 var store = {
   name: '1st and Pike',
   min: 10,
@@ -19,6 +19,13 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function arraySum(testArray) {
+  for(var i = 0; i < testArray.length; i++){
+    arrayTotal = testArray[i] + arrayTotal;
+  }
+  return arrayTotal;
+};
+
 for (var i = 0; i < store.hoursOpen.length; i++) {
   var randomNum = getRandomIntInclusive(store.min, store.max);
   store.hourlyCustomers[i] = randomNum;
@@ -26,11 +33,16 @@ for (var i = 0; i < store.hoursOpen.length; i++) {
 }
 var hourlyData = document.getElementsByTagName('ul');
 
-for(var i = 0; i < store.hoursOpen.length; i++) {
-  var hourlyInfo = document.createElement('li');
-  hourlyInfo.textContent = 'hour: ' + store.hoursOpen[i] + ' ~  customers: ' + store.hourlyCustomers[i] + ' ~ cookies ' + store.hourlyCookies[i];
-  hourlyData[0].appendChild(hourlyInfo);
+for(var i = 0; i <= store.hoursOpen.length; i++) {
+  if(i < store.hoursOpen.length){
+    var hourlyInfo = document.createElement('li');
+    hourlyInfo.textContent = 'hour: ' + store.hoursOpen[i] + ' ~  customers: ' + store.hourlyCustomers[i] + ' ~ cookies ' + store.hourlyCookies[i];
+    hourlyData[0].appendChild(hourlyInfo);
+  } else {
+    var hourlyInfo = document.createElement('li');
+    hourlyInfo.textContent = 'Total Customers: ' + arraySum(store.hourlyCustomers) + ' ~  Total Cookies ' + arraySum(store.hourlyCookies);
+    hourlyData[0].appendChild(hourlyInfo);
+  }
 }
-
 console.log(store.hourlyCustomers);
 console.log(store.hourlyCookies);
