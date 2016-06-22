e2395e1423c4a15cc4dc3f2816526218407978a1
dougachrist <dougachrist@gmail.com>
2016-06-21 18:20:15 -0700

    updated for functions and methods

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-// global varibles
+
 var arrayTotal = 0;
 var hoursOpen = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 var grandTotalCookies = 0;
@@ -14,7 +14,6 @@ function Store(storeName,minCustPerHour,maxCustPerHour,avgCookiesPerHour) {
   this.totalCookies = 0;
   this.totalCustomers = 0;
 }
-
 var storePike = new Store('1st and Pike',23,65,6.3);
 var storeSeaTac = new Store('SeaTac Airport',3,24,1.2);
 var storeSeattleCenter = new Store('Seattle Center',11,38,3.7);
@@ -41,59 +40,53 @@ Store.prototype.calCookiesPerHour = function() {
   }
 };
 
-// storeAlki.calCookiesPerHour();
-// console.log('table cookies', storeAlki.custPerHourArray);
+var allStores = [storePike, storeSeaTac, storeSeattleCenter, storeCapHill, storeAlki];
+for(var j = 0; j < allStores.length; j++) {
+  allStores[j].calCookiesPerHour();
+}
 
-var renderStoreTable = function() {
-  var allStores = [storePike, storeSeaTac, storeSeattleCenter, storeCapHill, storeAlki];
-  for(var j = 0; j < allStores.length; j++) {
-    allStores[j].calCookiesPerHour();
-  }
+var renderHeaderTable = function() {
   var tableData = document.getElementById('cookieTable');
   var headerRow = document.createElement('tr');
-// add first two th here:
   var blankData = document.createElement('th');
   blankData.textContent = '';
   headerRow.appendChild(blankData);
-
   var tableCookieTotals = document.createElement('th');
   tableCookieTotals.textContent = 'Daily Location Total';
   headerRow.appendChild(tableCookieTotals);
-
   for(var i = 0; i < hoursOpen.length; i++) {
     var tableEachHour = document.createElement('th');
     tableEachHour.textContent = hoursOpen[i];
     headerRow.appendChild(tableEachHour);
   }
   tableData.appendChild(headerRow);
-
-  // ADDS COOKIE DATA HERE
-
-  for(var j = 0; j < allStores.length; j++) {
-    var tableDataRow = document.createElement('tr');
-    var tableStoreName = document.createElement('td');
-    tableStoreName.textContent = allStores[j].storeName;
-    tableDataRow.appendChild(tableStoreName);
-
-    var tableStoreTotal = document.createElement('td');
-    tableStoreTotal.textContent = allStores[j].totalCookies;
-    grandTotalCookies += allStores[j].totalCookies;
-
-    tableDataRow.appendChild(tableStoreTotal);
-    for(var i = 0; i < hoursOpen.length; i++) {
-      var tableStoreData = document.createElement('td');
-      tableStoreData.textContent = allStores[j].cookiesPerHourArray[i];
-      tableDataRow.appendChild(tableStoreData);
-    }
-    tableData.appendChild(tableDataRow);
+};
+Store.prototype.renderStandData = function() {
+  var tableData = document.getElementById('cookieTable');
+  var tableDataRow = document.createElement('tr');
+  var tableStoreName = document.createElement('td');
+  tableStoreName.textContent = this.storeName;
+  tableDataRow.appendChild(tableStoreName);
+  var tableStoreTotal = document.createElement('td');
+  tableStoreTotal.textContent = this.totalCookies;
+  tableDataRow.appendChild(tableStoreTotal);
+  for(var i = 0; i < hoursOpen.length; i++) {
+    var tableStoreData = document.createElement('td');
+    tableStoreData.textContent = this.cookiesPerHourArray[i];
+    tableDataRow.appendChild(tableStoreData);
   }
-
-  // TOTALS
+  tableData.appendChild(tableDataRow);
+};
+var renderFooterTable = function() {
+  var tableData = document.getElementById('cookieTable');
   var tableDataRow = document.createElement('tr');
   var totalCellName = document.createElement('td');
   totalCellName.textContent = 'Totals';
   tableDataRow.appendChild(totalCellName);
   var grandTotalCell = document.createElement('td');
+  for(j = 0; j < allStores.length; j++){
+    grandTotalCookies += allStores[j].totalCookies;
+  }
   grandTotalCell.textContent = grandTotalCookies;
   tableDataRow.appendChild(grandTotalCell);
   for(var i = 0; i < hoursOpen.length; i++) {
@@ -106,6 +99,12 @@ var renderStoreTable = function() {
     tableDataRow.appendChild(totalCookiesByHour);
   }
   tableData.appendChild(tableDataRow);
-
 };
-renderStoreTable();
+
+renderHeaderTable();
+storePike.renderStandData();
+storeSeaTac.renderStandData();
+storeSeattleCenter.renderStandData();
+storeCapHill.renderStandData();
+storeAlki.renderStandData();
+renderFooterTable();
