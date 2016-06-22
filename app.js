4a69a443db3def9013170e1235d5f40a5bb53ba3
dougachrist <dougachrist@gmail.com>
2016-06-22 13:13:30 -0700

    dry code

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 var arrayTotal = 0;
 var hoursOpen = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 var grandTotalCookies = 0;
+var tableData = document.getElementById('cookieTable');
+var allStores = [];
 
 function Store(storeName,minCustPerHour,maxCustPerHour,avgCookiesPerHour) {
   this.storeName = storeName;
@@ -13,12 +15,9 @@ function Store(storeName,minCustPerHour,maxCustPerHour,avgCookiesPerHour) {
   this.cookiesPerHourArray = [];
   this.totalCookies = 0;
   this.totalCustomers = 0;
+  this.calCookiesPerHour();
+  allStores.push(this);
 }
-var storePike = new Store('1st and Pike',23,65,6.3);
-var storeSeaTac = new Store('SeaTac Airport',3,24,1.2);
-var storeSeattleCenter = new Store('Seattle Center',11,38,3.7);
-var storeCapHill = new Store('Capitol Hill',20,38,2.3);
-var storeAlki = new Store('Alki',2,16,4.6);
 
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -40,13 +39,13 @@ Store.prototype.calCookiesPerHour = function() {
   }
 };
 
-var allStores = [storePike, storeSeaTac, storeSeattleCenter, storeCapHill, storeAlki];
-for(var j = 0; j < allStores.length; j++) {
-  allStores[j].calCookiesPerHour();
-}
+var storePike = new Store('1st and Pike',23,65,6.3);
+var storeSeaTac = new Store('SeaTac Airport',3,24,1.2);
+var storeSeattleCenter = new Store('Seattle Center',11,38,3.7);
+var storeCapHill = new Store('Capitol Hill',20,38,2.3);
+var storeAlki = new Store('Alki',2,16,4.6);
 
 var renderHeaderTable = function() {
-  var tableData = document.getElementById('cookieTable');
   var headerRow = document.createElement('tr');
   var blankData = document.createElement('th');
   blankData.textContent = '';
@@ -62,7 +61,6 @@ var renderHeaderTable = function() {
   tableData.appendChild(headerRow);
 };
 Store.prototype.renderStandData = function() {
-  var tableData = document.getElementById('cookieTable');
   var tableDataRow = document.createElement('tr');
   var tableStoreName = document.createElement('td');
   tableStoreName.textContent = this.storeName;
@@ -78,7 +76,6 @@ Store.prototype.renderStandData = function() {
   tableData.appendChild(tableDataRow);
 };
 var renderFooterTable = function() {
-  var tableData = document.getElementById('cookieTable');
   var tableDataRow = document.createElement('tr');
   var totalCellName = document.createElement('td');
   totalCellName.textContent = 'Totals';
