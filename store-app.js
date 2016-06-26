506b31aa6bb60558adbdfd002c26ef42d62b6d82
dougachrist <dougachrist@gmail.com>
2016-06-26 15:37:22 -0700

    local storage mini update

--- store-app.js
+++ store-app.js
@@ -8,31 +8,41 @@ var wPage = document.getElementById('page');
 
 webpage = wPage.textContent;
 
-var updateOrderForm = function() {
-  if (webpage === 'Order Process') {
-    var productProcess = document.getElementById('product');
-    productProcess.textContent = product;
-  }
-};
+// var updateOrderForm = function() {
+//   if (webpage === 'Order Processing') {
+//     var productProcess = document.getElementById('product');
+//     productProcess.textContent = product;
+//   }
+// };
 
-if(webpage === 'Store') {
+// if(webpage === 'Store') {
 
+if(document.getElementById('orderForm')) {
   var form = document.getElementById('orderForm');
 
   form.addEventListener('submit', function(event) {
     event.preventDefault();
     var elInput = document.getElementById('productInput');
     product = elInput.value;
+    localStorage.setItem('productType',product);
     console.log('product:',product);
-    updateOrderForm();
+    // updateOrderForm();
   });
 }
 
 console.log('product:',product);
 
-var updateOrderForm = function() {
-  if (webpage === 'Order Process') {
+if (webpage === 'Order Processing') {
+  var productProcess = document.getElementById('product');
+  productProcess.textContent = localStorage.getItem('productType');
+
+
+  var form = document.getElementById('processingForm');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('product:',product);
     var productProcess = document.getElementById('product');
-    productProcess.textContent = product;
-  }
-};
+    productProcess.textContent = localStorage.getItem('productType');
+  });
+}
