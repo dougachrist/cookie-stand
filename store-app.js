56faa1398d8f7b79f7caac5af5aa93ea4e3717e3
dougachrist <dougachrist@gmail.com>
2016-06-26 16:27:56 -0700

    update order form"

--- store-app.js
+++ store-app.js
@@ -6,43 +6,60 @@ var elInput = '';
 
 var wPage = document.getElementById('page');
 
+localStorage.setItem('productType','');
+localStorage.setItem('orderQuantity','');
+localStorage.setItem('custName','');
+localStorage.setItem('custAddress','');
+
 webpage = wPage.textContent;
 
-// var updateOrderForm = function() {
-//   if (webpage === 'Order Processing') {
-//     var productProcess = document.getElementById('product');
-//     productProcess.textContent = product;
-//   }
-// };
-
-// if(webpage === 'Store') {
-
-if(document.getElementById('orderForm')) {
+// GRAB order details form Order page
+if(webpage === 'Store') {
   var form = document.getElementById('orderForm');
 
   form.addEventListener('submit', function(event) {
     event.preventDefault();
     var elInput = document.getElementById('productInput');
     product = elInput.value;
+    var elInput = document.getElementById('productQuantity');
+    quantity = elInput.value;
+    var elInput = document.getElementById('custName');
+    custName = elInput.value;
+    var elInput = document.getElementById('custAddress');
+    custAddress = elInput.value;
     localStorage.setItem('productType',product);
+    localStorage.setItem('orderQuantity',quantity);
+    localStorage.setItem('custName',custName);
+    localStorage.setItem('custAddress',custAddress);
     console.log('product:',product);
-    // updateOrderForm();
   });
 }
-
-console.log('product:',product);
 
 if (webpage === 'Order Processing') {
   var productProcess = document.getElementById('product');
   productProcess.textContent = localStorage.getItem('productType');
-
+  var productProcess = document.getElementById('quantity');
+  productProcess.textContent = localStorage.getItem('orderQuantity');
+  var productProcess = document.getElementById('Name');
+  productProcess.textContent = localStorage.getItem('custName');
+  var productProcess = document.getElementById('Address');
+  productProcess.textContent = localStorage.getItem('custAddress');
 
   var form = document.getElementById('processingForm');
 
   form.addEventListener('submit', function(event) {
     event.preventDefault();
-    console.log('product:',product);
+
     var productProcess = document.getElementById('product');
     productProcess.textContent = localStorage.getItem('productType');
+    var productProcess = document.getElementById('product');
+    productProcess.textContent = localStorage.getItem('productType');
+    var productProcess = document.getElementById('quantity');
+    productProcess.textContent = localStorage.getItem('orderQuantity');
+    var productProcess = document.getElementById('Name');
+    productProcess.textContent = localStorage.getItem('custName');
+    var productProcess = document.getElementById('Address');
+    productProcess.textContent = localStorage.getItem('custAddress');
+
   });
 }
