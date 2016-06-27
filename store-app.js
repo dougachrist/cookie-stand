b2f03e88dad8ba48d597a9e5e0b2dbec328ddf03
dougachrist <dougachrist@gmail.com>
2016-06-26 17:31:23 -0700

    update order form

--- store-app.js
+++ store-app.js
@@ -27,39 +27,70 @@ if(webpage === 'Store') {
     custName = elInput.value;
     var elInput = document.getElementById('custAddress');
     custAddress = elInput.value;
+    var elInput = document.getElementById('custCity');
+    custCity = elInput.value;
+    var elInput = document.getElementById('custState');
+    custState = elInput.value;
+    var elInput = document.getElementById('custZip');
+    custZip = elInput.value;
+    var elInput = document.getElementById('custPaymentInfo');
+    custPaymentInfo = elInput.value;
+
     localStorage.setItem('productType',product);
     localStorage.setItem('orderQuantity',quantity);
     localStorage.setItem('custName',custName);
     localStorage.setItem('custAddress',custAddress);
-    console.log('product:',product);
+    localStorage.setItem('custCity',custCity);
+    localStorage.setItem('custState',custState);
+    localStorage.setItem('custZip',custZip);
+    localStorage.setItem('custPaymentInfo',custPaymentInfo);
+    // console.log('product:',product);
   });
 }
 
 if (webpage === 'Order Processing') {
-  var productProcess = document.getElementById('product');
-  productProcess.textContent = localStorage.getItem('productType');
-  var productProcess = document.getElementById('quantity');
-  productProcess.textContent = localStorage.getItem('orderQuantity');
-  var productProcess = document.getElementById('Name');
-  productProcess.textContent = localStorage.getItem('custName');
-  var productProcess = document.getElementById('Address');
-  productProcess.textContent = localStorage.getItem('custAddress');
+  // var productProcess = document.getElementById('product');
+  // productProcess.textContent = localStorage.getItem('productType');
+  // var productProcess = document.getElementById('quantity');
+  // productProcess.textContent = localStorage.getItem('orderQuantity');
+  // var productProcess = document.getElementById('Name');
+  // productProcess.textContent = localStorage.getItem('custName');
+  // var productProcess = document.getElementById('Address');
+  // productProcess.textContent = localStorage.getItem('custAddress');
 
-  var form = document.getElementById('processingForm');
+  var updateSales = document.getElementById('processingForm');
 
-  form.addEventListener('submit', function(event) {
+  updateSales.addEventListener('submit', function(event) {
     event.preventDefault();
 
     var productProcess = document.getElementById('product');
     productProcess.textContent = localStorage.getItem('productType');
-    var productProcess = document.getElementById('product');
-    productProcess.textContent = localStorage.getItem('productType');
     var productProcess = document.getElementById('quantity');
     productProcess.textContent = localStorage.getItem('orderQuantity');
-    var productProcess = document.getElementById('Name');
+    var productProcess = document.getElementById('name');
     productProcess.textContent = localStorage.getItem('custName');
-    var productProcess = document.getElementById('Address');
+    var productProcess = document.getElementById('address');
     productProcess.textContent = localStorage.getItem('custAddress');
+    var productProcess = document.getElementById('city');
+    productProcess.textContent = localStorage.getItem('custCity');
+    var productProcess = document.getElementById('state');
+    productProcess.textContent = localStorage.getItem('custState');
+    var productProcess = document.getElementById('zip');
+    productProcess.textContent = localStorage.getItem('custZip');
+    var productProcess = document.getElementById('paymentType');
+    productProcess.textContent = localStorage.getItem('custPaymentInfo');
 
   });
 }
+
+// if (webpage === 'Order Processing') {
+//
+//   // var openOrder = document.getElementsById('openOrder');
+  //
+  // openOrder.addEventListener('click', function() {
+  //   move to closed orders array
+
+  // var closedOrder = document.getElementsById('closedOrder');
+
+  // closedOrder.addEventListener('click', function() {
+  //   move back to open orders array
